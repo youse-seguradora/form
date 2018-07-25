c419e271ff87acb92c66d27a8238c531e0fdbe49
Júlio Cotta <juliocbcotta@gmail.com>
2018-07-25 02:39:09 -0300

    migrated validators to kotlin test

--- validators/src/test/kotlin/br/com/youse/forms/validators/MinLengthValidatorTest.kt
+++ validators/src/test/kotlin/br/com/youse/forms/validators/MinLengthValidatorTest.kt
@@ -23,25 +23,28 @@ SOFTWARE.
  */
 package br.com.youse.forms.validators
 
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
 
 class MinLengthValidatorTest {
     lateinit var validator: Validator<CharSequence>
-    @Before
+
+    @BeforeTest
     fun setup() {
         validator = MinLengthValidator("invalid min length", 3)
     }
 
     @Test
     fun shouldValidate() {
-        Assert.assertFalse(validator.isValid(""))
-        Assert.assertFalse(validator.isValid("1"))
-        Assert.assertFalse(validator.isValid("12"))
+        assertFalse(validator.isValid(""))
+        assertFalse(validator.isValid("1"))
+        assertFalse(validator.isValid("12"))
 
-        Assert.assertFalse(validator.isValid(" "))
-        Assert.assertTrue(validator.isValid("123"))
-        Assert.assertTrue(validator.isValid("1234"))
+        assertFalse(validator.isValid(" "))
+        assertTrue(validator.isValid("123"))
+        assertTrue(validator.isValid("1234"))
     }
 }
