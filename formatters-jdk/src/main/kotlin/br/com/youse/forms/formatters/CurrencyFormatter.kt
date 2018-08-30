c3fa6b992ca805f2b17ad03f4efdd1cf4fe7b20c
Júlio Cotta <juliocbcotta@gmail.com>
2018-08-30 02:54:13 -0300

    adds DateFormatter

--- formatters-jdk/src/main/kotlin/br/com/youse/forms/formatters/CurrencyFormatter.kt
+++ formatters-jdk/src/main/kotlin/br/com/youse/forms/formatters/CurrencyFormatter.kt
@@ -1,12 +1,11 @@
 package br.com.youse.forms.formatters
 
-import br.com.youse.forms.formatters.Formatter
 import java.text.NumberFormat
 import java.util.*
 
 class CurrencyFormatter(private val locale: Locale) : Formatter<Number> {
+    private val formatter = NumberFormat.getCurrencyInstance(locale)
     override fun format(input: Number): String {
-        val currencyFormatter = NumberFormat.getCurrencyInstance(locale)
-        return currencyFormatter.format(input)
+        return formatter.format(input)
     }
 }
