b8b16c030c4054225525af1bda86f0a57bfe0e2f
Júlio Cotta <juliocbcotta@gmail.com>
2018-09-29 02:30:26 -0300

    Makes DeferredObservableValue child of ObservableValidation to validate when a value change.

--- form-common/src/main/kotlin/br/com/youse/forms/form/models/DeferredObservableValue.kt
+++ form-common/src/main/kotlin/br/com/youse/forms/form/models/DeferredObservableValue.kt
@@ -1,3 +1,26 @@
+/**
+MIT License
+
+Copyright (c) 2018 Youse Seguros
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 package br.com.youse.forms.form.models
 
 import br.com.youse.forms.form.IObservableValue
@@ -8,7 +31,7 @@ import br.com.youse.forms.form.IObservableValue.ValueObserver
  * This class uses a nullable {@code value}, if an initial value is available
  * when the form is being build, use {@Link ObservableValue}.
  */
-class DeferredObservableValue<T> : IObservableValue<T> {
+class DeferredObservableValue<T> : ObservableValidation(), IObservableValue<T> {
     private var listener: ValueObserver<T>? = null
     private var value: T? = null
 
@@ -19,6 +42,7 @@ class DeferredObservableValue<T> : IObservableValue<T> {
         this.listener = valueObserver
         if (value != null) {
             valueObserver.onChange(value!!)
+            onValidate()
         }
     }
 
